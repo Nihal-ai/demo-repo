e43af696075a60d3347dc08155e22bbea95afb35
Nihal-ai <154442211+Nihal-ai@users.noreply.github.com>
2024-09-23 21:17:23 +0530

    test update

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,6 +1,10 @@
 CREATE SCHEMA DEMO;
-CREATE TABLE HELLO_WORLD
+CREATE or replace TABLE HELLO_WORLD
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
 );
+
+insert into demo_db.demo.hello_world values('t1','t2');
+
+select * from demo_db.demo.hello_world;
