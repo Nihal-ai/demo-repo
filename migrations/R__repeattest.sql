d74a9f483ff9e047589d80c21c6ac20545ef7ae3
Nihal-ai <154442211+Nihal-ai@users.noreply.github.com>
2024-09-27 20:50:00 +0530

    test R 2

--- migrations/R__repeattest.sql
+++ migrations/R__repeattest.sql
@@ -1,8 +1,8 @@
 use SCHEMA DEMO;
-CREATE or replace TABLE HELLO_WORLD_R
+CREATE or replace TABLE HELLO_WORLD_R2
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
 );
 
-insert into demo_db.demo.HELLO_WORLD_R values('t1','t2');
+insert into demo_db.demo.HELLO_WORLD_R2 values('t1','t2');
