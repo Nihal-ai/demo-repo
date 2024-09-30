7258e10204b35d24857786b6513cc37db3656b58
Nihal-ai <154442211+Nihal-ai@users.noreply.github.com>
2024-09-30 22:07:15 +0530

    Testing without schema

--- migrations/R__repeattest.sql
+++ migrations/R__repeattest.sql
@@ -1,10 +1,10 @@
 use SCHEMA DEMO;
 
-CREATE or replace TABLE HELLO_WORLD_R4
+CREATE or replace TABLE HELLO_WORLD_R5
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
   ,test_field VARCHAR
 );
 
-insert into demo_db.demo.HELLO_WORLD_R4 values('t1','t2','t3');
+insert into demo_db.demo.HELLO_WORLD_R5 values('t1','t2','t3');
