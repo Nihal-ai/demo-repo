444a46efbd51de07dc371b229221b07a098dea61
Nihal-ai <154442211+Nihal-ai@users.noreply.github.com>
2024-09-30 22:33:45 +0530

    Create table r6

--- migrations/R__repeattest.sql
+++ migrations/R__repeattest.sql
@@ -1,11 +1,11 @@
 use SCHEMA DEMO;
 
 
-CREATE or replace TABLE HELLO_WORLD_R5
+CREATE or replace TABLE HELLO_WORLD_R6
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
   ,test_field VARCHAR
 );
 
-insert into demo_db.demo.HELLO_WORLD_R5 values('t1','t2','t3');
+insert into demo_db.demo.HELLO_WORLD_R6 values('t1','t2','t3');
