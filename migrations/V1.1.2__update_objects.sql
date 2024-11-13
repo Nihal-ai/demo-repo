e21d09734ca46d311e876386e1da3099a05c81d9
Nihal-ai <154442211+Nihal-ai@users.noreply.github.com>
2024-11-13 22:17:15 +0530

    Update V1.1.2__update_objects.sql

--- migrations/V1.1.2__update_objects.sql
+++ migrations/V1.1.2__update_objects.sql
@@ -1,10 +1,9 @@
-use SCHEMA DEMO;
 CREATE or replace TABLE HELLO_WORLD
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
 );
 
-insert into demo_db.demo.hello_world values('t1','t2');
+insert into hello_world values('t1','t2');
 
-select * from demo_db.demo.hello_world;
+select * from hello_world;
